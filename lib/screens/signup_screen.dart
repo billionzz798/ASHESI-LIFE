1706c10219c3a9a6bdc9cd58763d4fa3b08082bd
Papa Badu <raybadu10@gmail.com>
2026-04-14 09:38:11 +0000

    Home screen firebase

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -200,7 +200,7 @@ class _SignupScreenState extends State<SignupScreen> {
                       ),
                       const SizedBox(height: 8),
                       DropdownButtonFormField<String>(
-                        initialValue: _selectedMajor,
+                        value: _selectedMajor,
                         decoration: const InputDecoration(
                           hintText: 'Select your major',
                           prefixIcon: Icon(Icons.school_outlined),
